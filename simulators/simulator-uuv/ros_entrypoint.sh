7e8884218c5bac89ebf0ddc41fe4877d682cc35f
Xing Yu Ng <ngxingyu@comp.nus.edu.sg>
2023-01-17 01:32:01 +0800

    Further split simulator image into base and non-base (custom bb repos) Update gh actions

--- simulators/simulator-uuv/ros_entrypoint.sh
+++ simulators/simulator-uuv/ros_entrypoint.sh
@@ -3,5 +3,6 @@ set -e
 
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash" --
-source "/root/catkin_ws/devel/setup.bash" --
+source "/root/uuv_ws/devel/setup.bash" --
+# source "/root/catkin_ws/devel/setup.bash" --
 exec "$@"
