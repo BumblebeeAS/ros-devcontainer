540f40c1d1b078f04db221f69c2134dc79e4a879
lpohsien <popopo4407@gmail.com>
2023-01-16 13:16:12 +0800

    Update dockerfiles

--- ros-index/select.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export IMAGE=$(yq '.ros.[] | select(has("image"))|.image' /index.yaml | fzf)
-echo "Updating docker-compose.yml to use ${IMAGE} ros base image ..."
-yq -i '.services.workspace.image = "ros-devcontainer:"+strenv(IMAGE)+"-dev"' /work/docker-compose.yml
-echo "done."
-echo "Your ros environment is ready."
-echo "Please enter 'docker-compose up' to launch your environment."
