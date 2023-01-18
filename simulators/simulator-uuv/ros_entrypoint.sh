4703ee54bb405e2d2ad1e4ddfee6bb92d3de153a
Xing Yu Ng <ngxingyu@comp.nus.edu.sg>
2023-01-18 12:59:44 +0800

    fix uuv / vrx ws user

--- simulators/simulator-uuv/ros_entrypoint.sh
+++ simulators/simulator-uuv/ros_entrypoint.sh
@@ -3,6 +3,5 @@ set -e
 
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash" --
-source "/root/uuv_ws/devel/setup.bash" --
-[ -d "/root/catkin_ws/devel/" ] && source "/root/catkin_ws/devel/setup.bash" --
+[ -d "~/catkin_ws/devel/" ] && source "~/catkin_ws/devel/setup.bash" --
 exec "$@"
