51445cd409ddc2288348505718ef513a76f30d22
Xing Yu Ng <ngxingyu@comp.nus.edu.sg>
2023-01-18 14:14:42 +0800

    update ros_entrypoint

--- simulators/simulator-uuv/ros_entrypoint.sh
+++ simulators/simulator-uuv/ros_entrypoint.sh
@@ -3,5 +3,5 @@ set -e
 
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash" --
-[ -d "~/catkin_ws/devel/" ] && source "~/catkin_ws/devel/setup.bash" --
+[ -d "/home/developer/catkin_ws/devel/" ] && source "/home/developer/catkin_ws/devel/setup.bash" --
 exec "$@"
--- simulators/simulator-vrx/ros_entrypoint.sh
+++ simulators/simulator-vrx/ros_entrypoint.sh
@@ -3,6 +3,5 @@ set -e
 
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash" --
-source "/root/vrx_ws/devel/setup.bash" --
-[ -d "/root/catkin_ws/devel/" ] && source "/root/catkin_ws/devel/setup.bash" --
+[ -d "/home/developer/catkin_ws/devel/" ] && source "/home/developer/catkin_ws/devel/setup.bash" --
 exec "$@"
