04d4625143650cbf6bc6f768ff992a01d22ce4f0
Xing Yu Ng <ngxingyu@comp.nus.edu.sg>
2023-01-19 12:56:35 +0800

    fix gazebosim dependency

--- simulators/simulator-vrx-gazebosim/ros_entrypoint.sh
+++ simulators/simulator-vrx-gazebosim/ros_entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 # setup ros2 environment
-source "/opt/ros/$ROS_DISTRO/setup.bash" --
+source "/opt/ros/humble/setup.bash" --
 [ -d "/home/developer/colcon_ws/install/" ] && source "/home/developer/colcon_ws/install/setup.bash" --
 exec "$@"
