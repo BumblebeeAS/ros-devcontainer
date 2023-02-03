2f7f568ecc19c03ab1302a116a70732ce1580f36
Xing Yu Ng <ngxingyu@comp.nus.edu.sg>
2023-02-03 09:27:42 +0800

    update docker-compose.yml

--- select-env.sh
+++ select-env.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-# docker pull devrt/simulator-index
 docker pull ghcr.io/ngxingyu/select-index:latest
 docker run -ti --rm -v $(pwd):/work ghcr.io/ngxingyu/select-index:latest
-
-# docker run -ti --rm -v $(pwd):/work select-index:latest
